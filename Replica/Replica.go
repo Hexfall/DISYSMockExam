0499d380f80457703f239c0428b67750770492fc
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2022-01-03 23:03:01 +0100

    Implemented gRPC functions for replicas.

--- Replica/Replica.go
+++ Replica/Replica.go
@@ -1,1 +1,46 @@
-package Replica
+package main
+
+import (
+	"flag"
+	"fmt"
+	increment "github.com/Hexfall/DISYSMockExam/Increment"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+var port = flag.Int("port", 5080, "port")
+var target = flag.String("target", "", "target")
+
+var server Server
+
+func main() {
+	flag.Parse()
+	server = Server{
+		isLeader:   *target == "",
+		leaderAddr: *target,
+		selfAddr:   fmt.Sprintf(":%d", *port),
+		value:      -1,
+	}
+
+	// gRPC set-up.
+	log.Printf("Attempting to listen on port: %d\n", *port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen on port %d. Error: %v", *port, err)
+	} else {
+		log.Printf("Now listening on port %d.\n", *port)
+	}
+
+	var options []grpc.ServerOption
+	grpcServer := grpc.NewServer(options...)
+	increment.RegisterIncrementServiceServer(grpcServer, &server)
+
+	if *target != "" {
+		// TODO: Connect to leader node.
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC client on port %d. Error: %v", *port, err)
+	}
+}
--- Replica/Server.go
+++ Replica/Server.go
@@ -1,1 +1,88 @@
 package main
+
+import (
+	"context"
+	increment "github.com/Hexfall/DISYSMockExam/Increment"
+	"log"
+	"sync"
+)
+
+type Server struct {
+	increment.UnimplementedIncrementServiceServer
+
+	isLeader     bool
+	leaderAddr   string
+	selfAddr     string
+	replicas     []string
+	replicaMutex sync.Mutex
+
+	value int64
+	mutex sync.Mutex
+}
+
+// gRPC functions.
+
+func (s *Server) Increment(ctx context.Context, void *increment.VoidMessage) (*increment.IncrementMessage, error) {
+	if !s.isLeader {
+		// This replica doesn't have the authority to Increment.
+		return nil, &increment.ImpermissibleError{}
+	}
+
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.value++
+	log.Printf("Value has been incremented to %d.\n", s.value)
+	return &increment.IncrementMessage{Number: s.value}, nil
+}
+
+func (s *Server) GetLeader(ctx context.Context, void *increment.VoidMessage) (*increment.LeaderMessage, error) {
+	return &increment.LeaderMessage{
+		Ip:       s.leaderAddr,
+		IsLeader: s.isLeader,
+	}, nil
+}
+
+func (s *Server) GetReplicas(ctx context.Context, void *increment.VoidMessage) (*increment.ReplicaListMessage, error) {
+	return &increment.ReplicaListMessage{Ips: s.replicas}, nil
+}
+
+func (s *Server) Join(ctx context.Context, ipMessage *increment.IpMessage) (*increment.VoidMessage, error) {
+	if !s.isLeader {
+		return nil, &increment.ImpermissibleError{}
+	}
+
+	s.replicaMutex.Lock()
+	defer s.replicaMutex.Unlock()
+
+	s.replicas = append(s.replicas, ipMessage.Ip)
+	// TODO: Monitor heartbeat.
+
+	return &increment.VoidMessage{}, nil
+}
+
+func (s *Server) HeartBeat(ctx context.Context, void *increment.VoidMessage) (*increment.VoidMessage, error) {
+	return &increment.VoidMessage{}, nil
+}
+
+func (s *Server) SendReplicas(ctx context.Context, replicasMessage *increment.ReplicaListMessage) (*increment.VoidMessage, error) {
+	if s.isLeader {
+		// Leader cannot be ordered around.
+		return nil, &increment.ImpermissibleError{}
+	}
+	// Should really check whether sender is leader, somehow.
+	s.replicas = replicasMessage.Ips
+
+	return &increment.VoidMessage{}, nil
+}
+
+func (s *Server) SendValue(ctx context.Context, incrementMessage *increment.IncrementMessage) (*increment.VoidMessage, error) {
+	if s.isLeader {
+		// Leader cannot be ordered around.
+		return nil, &increment.ImpermissibleError{}
+	}
+	// Should really check whether sender is leader, somehow.
+	s.value = incrementMessage.Number
+
+	return &increment.VoidMessage{}, nil
+}
